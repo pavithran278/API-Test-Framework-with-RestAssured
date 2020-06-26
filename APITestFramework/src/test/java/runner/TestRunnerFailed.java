899b3187f06c09c049bdf5831797104b536d99d4
pavithran278 <51323478+pavithran278@users.noreply.github.com>
2020-06-26 10:32:23 +0530

    Added retry mechanism

--- APITestFramework/src/test/java/runner/TestRunnerFailed.java
+++ APITestFramework/src/test/java/runner/TestRunnerFailed.java
@@ -1,18 +1,18 @@
 package runner;
 
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/java/feature",
+        features = "@target/failedTest.txt",
         glue = {"steps"},
-        plugin = {"json:target/Result.json",
-                "rerun:target/failedTest.txt"},
+        plugin = {"json:target1/Result.json",
+                "rerun:target1/failedTest.txt"},
         monochrome = true
 )
 
-public class TestRunner {
-
+public class TestRunnerFailed {
 }
